b33add4fbde4f03f828c629169f017dcc6c87d0f
Till Johanndeiter <till.johanndeiter@web.de>
2022-04-10 16:36:11 +0200

    Adjust layout

--- common-app-covpass/src/main/java/de/rki/covpass/app/widget/CertificateWidget.kt
+++ common-app-covpass/src/main/java/de/rki/covpass/app/widget/CertificateWidget.kt
@@ -4,8 +4,14 @@ package de.rki.covpass.app.widget
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
+import com.ensody.reactivestate.DependencyAccessor
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.journeyapps.barcodescanner.BarcodeEncoder
 import de.rki.covpass.app.R
+import de.rki.covpass.app.dependencies.covpassDeps
 
 
 /**
@@ -15,25 +21,40 @@ public class CertificateWidget : AppWidgetProvider() {
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray
+        appWidgetIds: IntArray,
     ) {
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
-
-    override fun onEnabled(context: Context) {
-    }
-
-    override fun onDisabled(context: Context) {
-    }
 }
 
+@OptIn(DependencyAccessor::class)
 internal fun updateAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
+    appWidgetId: Int,
 ) {
     val views = RemoteViews(context.packageName, R.layout.certificate_widget)
+
+    val certs = covpassDeps.certRepository.certs.value
+
+    val cert = certs.certificates[0].getMainCertificate()
+
+    val qrContent = certs.certificates[0].getMainCertificate().qrContent
+    val bitmap =  BarcodeEncoder().encodeBitmap(
+        qrContent,
+        BarcodeFormat.QR_CODE,
+        context.resources.displayMetrics.widthPixels,
+        context.resources.displayMetrics.widthPixels,
+        mapOf(EncodeHintType.MARGIN to 0),
+    )
+
+    views.setImageViewBitmap(R.id.certificate_qr_imageview, bitmap)
+    views.setTextViewText(R.id.certificate_name_textview, cert.covCertificate.fullName)
+
+
+
+    // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
 }
